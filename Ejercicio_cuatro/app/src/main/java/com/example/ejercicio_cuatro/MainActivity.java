d6da07b092676a528af26980715145265d71dde0
Cristiangpbf <cristiangpbf@gmail.com>
2021-02-01 21:27:54 -0500

    Quinto ejercicio Buscaminas

--- Ejercicio_cuatro/app/src/main/java/com/example/ejercicio_cuatro/MainActivity.java
+++ Ejercicio_cuatro/app/src/main/java/com/example/ejercicio_cuatro/MainActivity.java
@@ -1,15 +1,16 @@
 package com.example.ejercicio_cuatro;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
-import android.provider.MediaStore;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     int peticion = 1;
@@ -36,5 +37,48 @@ public class MainActivity extends AppCompatActivity {
             url1 = data.getData();
         }
     }
-
 }
+
+//public class MainActivity extends AppCompatActivity {
+//    int peticion = 1;
+//    Uri url1;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu){
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item){
+//        int id=item.getItemId();
+//        if(id==R.id.action_settings){
+//            return true;
+//        }
+//    }
+//
+//    public void grabar(View v) {
+//        Intent intent = new Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION);
+//        startActivityForResult(intent, peticion);
+//    }
+//
+//    public void reproducir(View v) {
+//        MediaPlayer mediaPlayer = MediaPlayer.create(this, url1);
+//        mediaPlayer.start();
+//    }
+//
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+////        super.onActivityResult(requestCode, resultCode, data);
+//        if (resultCode == RESULT_OK && requestCode == peticion) {
+//            url1 = data.getData();
+//        }
+//    }
+//
+//}
+
